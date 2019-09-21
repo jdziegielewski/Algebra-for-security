e471260c7d9a7325560e4b3cfd5cca55e4b83d35
jdziegielewski <50102093+jdziegielewski@users.noreply.github.com>
2019-09-21 20:20:53 +0200

    add sub + (mod m) + multi

--- add_sub_mul/add_sub_mul.cpp
+++ add_sub_mul/add_sub_mul.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <fstream.h>
 using namespace std;
 
 int main()
@@ -77,7 +77,8 @@ int main()
 	}
 	if(number1[0]=='-' && number2[0]!='-' && operation=="multiplication"){ //multiplication of negative x and positive y
         number1.erase(0,1);
-        sign='-';
+        sign='-';	cout << " Input the radix: ";
+	cin>>radix;
 	}
 	if(number1[0]!='-' && number2[0]=='-' && operation=="multiplication"){ //multiplication of positive x and negative y
         number2.erase(0,1);
@@ -362,36 +363,41 @@ else if(operation=="multiplication"){
     //modular arithmetics
 
       case 2: {
-      string m;//modulus
+    string operation1;
+    string m;//modulus
     string x;//x
     string y;//y
     string result1;//result
     int radix;
     cout << " Input the radix: ";
 	cin>>radix;
+	cout<<"Operation :";
+	cin>>operation1;
 	cout << " Input the 1st number: ";
 	cin>> x;
 	cout << " Input the 2nd number: ";
 	cin>> y;
 	cout << " Input the modulo: ";
 	cin>> m;
-    int r;
-    int p=m.size();
+    int r; //carry (remainder)
+    char sign='+';
+    int p=m.size(); //word length of m,x,y;
     int k=x.size();
     int n=y.size();
-    int len=p+1;
+    int len=p+1; //p+1 because of addition
+    // declaring arrays of int to compute the operations
     int* num1 = new int[ len +1];
     int* num2 = new int[ len +1];
     int* modulo = new int[ len +1];
     int* zprime = new int [ len +1];
     int* result = new int [ len+1 ];
 
-    for(int i=0;i<len+1;i++){
+    for(int i=0;i<len+1;i++){ //first fitting the arrays with 0's so if the numbers are different in word size the operation will be held
         num1[i]=0;
         num2[i]=0;
         modulo[i]=0;
     }
-
+//reversing the strings to arrays of int for x and y and m
     for(int i=k-1,j=0; i>=0; i--,j++){
 
             if(x[j]=='A'){
@@ -400,7 +406,7 @@ else if(operation=="multiplication"){
             else if(x[j]=='B'){
                 num1[i]=11;
             }
-            else if(x[j]=='C'){
+            else if(x[j]=='C') {
                 num1[i]=12;
             }
             else if(x[j]=='D'){
@@ -466,9 +472,17 @@ else if(operation=="multiplication"){
 
     }
     //now x,y,m are reversed
-
-//step 1.1
-for(int i=0;i<len+1;i++){
+    // needed if's because switch case does not work with strings
+    int opr;
+    if(operation1=="addition"){
+        opr=1;
+    }
+    else if(operation1=="subtraction"){
+        opr=2;
+    }
+switch(opr){
+case 1: { // modular addition algorithm
+    for(int i=0;i<len+1;i++){
         zprime[i]=num1[i]+num2[i]+r;
         if(zprime[i]>=radix){
             zprime[i]=zprime[i]-radix;
@@ -512,7 +526,7 @@ else if(smaller==false){
     }
 
 }
-  for(int i=len,j=0; i>=0; i--,j++){
+  for(int i=len,j=0; i>=0; i--,j++){ //reversing the array of int to string
 
             if(result[i]==10){
                 result1[j]='A';
@@ -537,6 +551,7 @@ else if(smaller==false){
 
 
     }
+    // outputting the answer
     int c=-1;
     cout<<"[answer] : ";
     for(int i=0;i<len+1;i++){
@@ -545,14 +560,105 @@ else if(smaller==false){
                     c++;
             }
             if(c!=i){
-        cout<< result1[i]; //result reversed result1[i]
+        cout<< result1[i];
     }
     }
 
     break;
-      }
+}//end mod add case
+case 2: { // modular subtraction
+ for(int i=len; i>=0; i--){
+            if(num1[i]>num2[i]){
+                break;
+            }
+            else if (num1[i]<num2[i]){//case when we should reverse the numbers
+           std::swap(num1, num2);
+                sign='-';
+                break;
+            }
+
+          }
+  for(int i=0;i<len+1;i++){ // algorithm for subtraction (mod m)
+        zprime[i]=num1[i]-num2[i]-r;
+        if(zprime[i]<0){
+            zprime[i]=zprime[i]+radix;
+            r=1;
+        }
+        else r=0;
+    }
+
+
+//check which is bigger
+bool smaller;
+            if(sign=='+'){
+                smaller=true;
+            }
+            else smaller=false;
+
+
+if(smaller==true){
+   for(int i=0;i<len+1;i++){
+        result[i]=zprime[i];
+   }
+}
+else{
+    r=0;
+    for(int i=0;i<len+1;i++){
+        result[i]=modulo[i]-zprime[i]-r;
+        if(result[i]<0){
+            result[i]=result[i]+radix;
+            r=1;
+        }
+        else r=0;
+    }
+
+}
+  for(int i=len,j=0; i>=0; i--,j++){ //reversing the array of int to string
+
+            if(result[i]==10){
+                result1[j]='A';
+            }
+            else if(result[i]==11){
+                result1[j]='B';
+            }
+            else if(result[i]==12){
+                result1[j]='C';
+            }
+            else if(result[i]==13){
+                result1[j]='D';
+            }
+            else if(result[i]==14){
+                result1[j]='E';
+            }
+            else if(result[i]==15){
+                result1[j]='F';
+            }
+            else
+            result1[j] = result[i]+48;
+
 
     }
+    // outputting the answer
+    int c=-1;
+    cout<<"[answer] : ";
+    for(int i=0;i<len+1;i++){
+
+            if(result1[i]=='0'){
+                    c++;
+            }
+            if(c!=i){
+        cout<< result1[i];
+    }
+    }
+}
+}
+//step 1.1
+
+      }//end of modular case
+
+      }//end of integer / modular switch case
+
+
 
 
 
