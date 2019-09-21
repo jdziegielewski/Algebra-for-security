6d92fa6a514ea68b8479f318d6d540b3259c8a75
jdziegielewski <50102093+jdziegielewski@users.noreply.github.com>
2019-09-21 17:27:38 +0200

    new

--- add_sub_mul/add_sub_mul.cpp
+++ add_sub_mul/add_sub_mul.cpp
@@ -4,7 +4,20 @@ using namespace std;
 
 int main()
 {
-    string operation;
+    string arithmetic;
+    int whicharithmetic;
+    cout << " Input the arithmetic: ";
+	cin>>arithmetic;
+
+    if(arithmetic=="integer"){
+        whicharithmetic=1;
+    }
+    else if(arithmetic=="modular"){
+        whicharithmetic=2;
+    }
+    switch(whicharithmetic) {
+        case 1: {
+            string operation;
     string number1;
 	string number2;
 	string result1;
@@ -21,6 +34,7 @@ int main()
 	cout << " Input the 2nd number: ";
 	cin>> number2;
 
+	//checking th
 
 	if(number1[0]=='-' && number2[0]=='-' && operation=="addition"){ //case 3
         number1.erase(0,1);
@@ -144,7 +158,7 @@ else if(operation=="multiplication"){
     switchcase=3;
 }
  switch(switchcase) {
-      case 1 :
+      case 1 : {
           for(int i=0;i<len;i++){
         result[i]=num1[i]+num2[i]+r;
         if(result[i]>=radix){
@@ -188,8 +202,8 @@ else if(operation=="multiplication"){
         cout<< result1[i]; //result reversed result1[i]
     }
          break;
-
-      case 2 :
+      }
+      case 2 : {
                for(int i=len-1; i>=0; i--){
             if(num1[i]>num2[i]){
                 break;
@@ -210,10 +224,7 @@ else if(operation=="multiplication"){
         }
         else r=0;
     }
-    k=m;
-    while(k>=2 && result[k-1]==0){
-        k=k-1;
-    }
+
 
     for(int i=len-1,j=0; i>=0; i--,j++){
 
@@ -244,13 +255,20 @@ else if(operation=="multiplication"){
      cout<<"\n";
      if(sign=='-'){
         cout<<sign;}
-         for(int i=0;i<len;i++){
+int a=-1;
+    for(int i=0;i<len;i++){
+        if(result1[i]=='0'){
+            a++;
+        }
+        if(a!=i){
         cout<< result1[i]; //result reversed result1[i]
     }
+    }
          break;
+    }
 
 
-      case 3:
+      case 3: {
         int t;
         for (int i=0; i<m+n-1; i++)
         {
@@ -306,4 +324,202 @@ else if(operation=="multiplication"){
         break;
 
    }
+}
+
+    }
+      case 2: {
+      string m;//modulus
+  string x;//x
+  string y;//y
+  string result1;//result
+  int radix;
+  cout << "\n\n Modular addition:\n";
+	cout << "-----------------------------------\n";
+	cout << " Input the 1st number: ";
+	cin>> x;
+	cout << " Input the 2nd number: ";
+	cin>> y;
+	cout << " Input the modulo: ";
+	cin>> m;
+	cout << " Input the radix: ";
+	cin>>radix;
+  int r;arithmetic
+    int p=m.size();
+    int k=x.size();
+    int n=y.size();
+    int len=p+1;
+    int* num1 = new int[ len +1];
+    int* num2 = new int[ len +1];
+    int* modulo = new int[ len +1];
+    int* zprime = new int [ len +1];
+    int* result = new int [ len+1 ];
+
+    for(int i=0;i<len+1;i++){
+        num1[i]=0;
+        num2[i]=0;
+        modulo[i]=0;
+    }
+
+    for(int i=k-1,j=0; i>=0; i--,j++){
+
+            if(x[j]=='A'){
+                num1[i]=10;
+            }
+            else if(x[j]=='B'){
+                num1[i]=11;
+            }
+            else if(x[j]=='C'){
+                num1[i]=12;
+            }
+            else if(x[j]=='D'){
+                num1[i]=13;
+            }
+            else if(x[j]=='E'){
+                num1[i]=14;
+            }
+            else if(x[j]=='F'){
+                num1[i]=15;
+            }
+            else
+            num1[i] = x[j]-48;
+
+
+    }
+     for(int i=n-1,j=0; i>=0; i--,j++){
+
+            if(y[j]=='A'){
+                num2[i]=10;
+            }
+            else if(y[j]=='B'){
+                num2[i]=11;
+            }
+            else if(y[j]=='C'){
+                num2[i]=12;
+            }
+            else if(y[j]=='D'){
+                num2[i]=13;
+            }
+            else if(y[j]=='E'){
+                num2[i]=14;
+            }
+            else if(y[j]=='F'){
+                num2[i]=15;
+            }
+            else
+            num2[i] = y[j]-48;
+
+    }
+         for(int i=p-1,j=0; i>=0; i--,j++){
+
+            if(m[j]=='A'){
+                modulo[i]=10;
+            }
+            else if(m[j]=='B'){
+                modulo[i]=11;
+            }
+            else if(m[j]=='C'){
+                modulo[i]=12;
+            }
+            else if(m[j]=='D'){
+                modulo[i]=13;
+            }
+            else if(m[j]=='E'){
+                modulo[i]=14;
+            }
+            else if(m[j]=='F'){
+                modulo[i]=15;
+            }
+            else
+            modulo[i] = m[j]-48;
+
+    }
+    //now x,y,m are reversed
+
+//step 1.1
+for(int i=0;i<len+1;i++){
+        zprime[i]=num1[i]+num2[i]+r;
+        if(zprime[i]>=radix){
+            zprime[i]=zprime[i]-radix;
+            r=1;
+        }
+        else r=0;
+}
+if(r==1){
+    zprime[len]=1;
+}
+
+//check which is bigger
+bool smaller;
+     for(int i=len; i>=0; i--){
+            if(modulo[i]>zprime[i]){
+                smaller=true;
+                break;
+            }
+            else if (modulo[i]<zprime[i]){//case when we should reverse the numbers
+                smaller=false;
+                break;
+            }
+
+          }
+
+if(smaller==true){
+   for(int i=0;i<len+1;i++){
+        result[i]=zprime[i];
+   }
+}
+else if(smaller==false){
+    r=0;
+    for(int i=0;i<len+1;i++){
+        result[i]=zprime[i]-modulo[i]-r;
+        if(result[i]<0){
+            result[i]=result[i]+radix;
+            r=1;
+        }
+        else r=0;
+    }
+
+}
+  for(int i=len,j=0; i>=0; i--,j++){
+
+            if(result[i]==10){
+                result1[j]='A';
+            }
+            else if(result[i]==11){
+                result1[j]='B';
+            }
+            else if(result[i]==12){
+                result1[j]='C';
+            }
+            else if(result[i]==13){
+                result1[j]='D';
+            }
+            else if(result[i]==14){
+                result1[j]='E';
+            }
+            else if(result[i]==15){
+                result1[j]='F';
+            }
+            else
+            result1[j] = result[i]+48;
+
+
+    }
+    int c=-1;
+    for(int i=0;i<len+1;i++){
+
+            if(result[i]==0){
+                    c++;
+            }
+            if(c!=i){
+        cout<< result1[i]; //result reversed result1[i]
+    }
+    }
+      }
+
+    }
+
+
+
+
+
 }
