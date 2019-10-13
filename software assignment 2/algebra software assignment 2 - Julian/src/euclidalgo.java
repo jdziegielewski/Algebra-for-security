ed83ca0abe7126d8fd03f3b24ac63d49db8a8c0e
Lyuben <50165908+lyubeka99@users.noreply.github.com>
2019-10-13 20:33:00 +0200

    a

--- software assignment 2/algebra software assignment 2 - Julian/src/euclidalgo.java
+++ software assignment 2/algebra software assignment 2 - Julian/src/euclidalgo.java
@@ -2,4 +2,23 @@ public class euclidalgo {
     public int[] euclidAlgo(int[] a,int[] b) {
 
     }
+
+    public int[] divwsubtraction (int[] a, int[] b) {
+        polynomialsubtraction divObj = new polynomialsubtraction();
+
+        //for-cycle (dega > degb) {do polynomialsubtraction() count++)
+        //divObj.polysubtraction(a,b, 10);
+
+    }
+
+    public int finddegree(int[] a) {
+        int degA = a.length;
+
+        for (int i = a.length; i <= 0; i-- ) {
+            if (a[i-1]==0) {
+                degA = degA - 1;
+            }
+        }
+        return degA;
+    }
 }
