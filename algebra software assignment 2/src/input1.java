e81916cd1fd8486afc07372d286faf978bf099c0
Lyuben <50165908+lyubeka99@users.noreply.github.com>
2019-10-26 17:12:54 +0200

    polymulti

--- algebra software assignment 2/src/input1.java
+++ algebra software assignment 2/src/input1.java
@@ -27,6 +27,7 @@ public class input1 {
 	polynomialsubtraction subpolyObject = new polynomialsubtraction();
 	longdivision longdivisObject = new longdivision();
 	polyequalspoly polyeqpolyObject = new polyequalspoly();
+	polynomialmultiplication polymultiObject = new polynomialmultiplication();
 	
 	File file = new File("example.txt");
 	Scanner scan = new Scanner(file);
@@ -150,7 +151,7 @@ public class input1 {
 		  displayObject.displaypoly(subpolyObject.polysubtraction(convertpolyObject.polyconversion(f,mod),convertpolyObject.polyconversion(g,mod),mod));
 		break;
 	  case 4://[multiply-poly]
-		//function should be here
+		displayObject.displaypoly(polymultiObject.polymultiplication(convertpolyObject.polyconversion(f,mod),convertpolyObject.polyconversion(g,mod),mod));
 		break;
 	  case 5://[long-div-poly]
 		  displaylongdivObject.displaypolyld(longdivisObject.longdiv(convertpolyObject.polyconversion(f,mod),convertpolyObject.polyconversion(g,mod),mod));
